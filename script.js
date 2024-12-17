214c5958bcaa195bb9af1d6e5d357516bd35d42f
anushka aryal <aryalanushka2020@gmail.com>
2024-12-18 01:31:23 +0545

    responsive navbar

--- script.js
+++ script.js
@@ -82,3 +82,66 @@ function setActiveAccordion(activeIndex) {
     content.classList.toggle("show", index === activeIndex);
   });
 }
+
+
+const hamburger = document.getElementById('hamburger');
+const navListMob = document.getElementById('navList-mob');
+const navItemsMob = document.querySelectorAll('.nav-item-mob');
+const backArrowMob = document.getElementById('backArrow-mob');
+const logo = document.querySelector('.logo h1');
+const menuIcon = document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.close-icon');
+
+
+hamburger.addEventListener('click', () => {
+    const isOpen = navListMob.classList.contains('active'); 
+    if (isOpen) {
+        navListMob.classList.remove('active'); 
+        menuIcon.style.display = 'inline-block'; 
+        closeIcon.style.display = 'none'; 
+        backArrowMob.style.display = 'none';
+    } else {
+        navListMob.classList.add('active'); 
+        menuIcon.style.display = 'none'; 
+        closeIcon.style.display = 'inline-block'; 
+    }
+});
+
+
+navItemsMob.forEach(item => {
+    item.addEventListener('click', (event) => {
+        
+        navItemsMob.forEach(navItemsMob => navItemsMob.classList.remove('active'));
+
+        item.classList.add('active');
+       
+        const linkText = item.innerText;
+        logo.innerText = linkText;
+
+        backArrowMob.style.display = 'inline-block';
+        
+        const target = document.querySelector(item.getAttribute('href'));
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop - 50, // 
+                behavior: 'smooth',
+            });
+        }
+
+        navListMob.classList.remove('active');
+        hamburger.classList.remove('open'); 
+    });
+});
+
+
+backArrowMob.addEventListener('click', () => {
+  
+    logo.innerText = "Logo";
+
+    backArrowMob.classList.remove('active');
+
+    navListMob.classList.add('active');
+    hamburger.classList.add('open');
+});
+
+
